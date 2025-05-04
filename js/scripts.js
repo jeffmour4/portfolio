308746c98bf0e3f2f887f22f145f010708e0c85b
jeffmour4 <jefflbmoura@gmail.com>
2025-05-04 14:30:30 -0300

    Adição de formulário de contato na página contatos e alerta no javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 //função para alertar o usuário ao clicar em Início quando em uma página diferente da inicial
 function mensagemInicio() {
-    if (location.href != "http://localhost/portfolio/index.html") {
+    if (location.href != "hhttps://jeffmour4.github.io/portfolio/index.html") {
         alert("Voltando ao início...");
         location.href = "index.html";
     }
+}
+
+//função para alertar acerca da não implementação do formulário de contato
+function trabalhandoNisso() {
+    alert("Desculpe. Ainda não implementado.")
 }
